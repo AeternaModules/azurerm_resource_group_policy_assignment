90a61e95844eb4546429cb6486f5cf2b77a1ffab
Daniel Allen <dan@aeternagroup.net>
2026-01-11 17:59:16 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "resource_group_policy_assignments" {
+  description = "All resource_group_policy_assignment resources"
+  value       = azurerm_resource_group_policy_assignment.resource_group_policy_assignments
+}
+
